e905ae269fc7d3de99bf32b9086b42dc7383f808
Brokenloon361987 <67371601+Brokenloon361987@users.noreply.github.com>
2020-07-28 18:05:51 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,69 @@
+var helicopterIMG, helicopterSprite, packageSprite,packageIMG;
+var packageBody,ground
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+var bassist1;
+var Wall1,Wall2;
+function preload()
+{
+	helicopterIMG=loadImage("helicopter.png")
+	packageIMG=loadImage("package.png")
+}
+
+function setup() {
+	createCanvas(800, 700);
+	rectMode(CENTER);
+	
+
+	packageSprite=createSprite(width/2, 80, 10,10);
+	packageSprite.addImage(packageIMG)
+	packageSprite.scale=0.2
+
+	helicopterSprite=createSprite(width/2, 200, 10,10);
+	helicopterSprite.addImage(helicopterIMG)
+	helicopterSprite.scale=0.6
+
+	groundSprite=createSprite(width/2, height-35, width,10);
+	groundSprite.shapeColor=color(255)
+
+	engine = Engine.create();
+	world = engine.world;
+
+	packageBody = Bodies.circle(width/2 , 200 , 5 , {restitution:0, isStatic:true});
+	World.add(world, packageBody);
+	
+
+	//Create a Ground
+	ground = Bodies.rectangle(width/2, 650, width, 10 , {isStatic:true} );
+ 	World.add(world, ground);
+
+
+	Engine.run(engine);
+  
+	bassist1 = new Bass(400, 655, 200, 10);
+	Wall1 = new WALL(300,645,10,30);
+	Wall2 = new WALL(500,645,10,30);
+}
+
+
+function draw() {
+  rectMode(CENTER);
+  background(0);
+  packageSprite.x= packageBody.position.x 
+  packageSprite.y= packageBody.position.y 
+  drawSprites();
+  bassist1.display();
+  Wall1.display();
+  Wall2.display();
+}
+
+function keyPressed() {
+if(helicopterSprite.x>350 && helicopterSprite.x<450){	
+ if (keyCode === DOWN_ARROW) {
+    //matter.body.setPosision
+    Matter.Body.setStatic(packageBody,false);
+  }
+}
+}
